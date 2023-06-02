5c69f01686045fc385bdab1aff9444860b1a6578
Chang-Jun Jeon <86146128+ondjj@users.noreply.github.com>
2023-06-02 21:28:36 +0900

    [Bronze V] Title: 아스키 코드, Time: 200 ms, Memory: 17604 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String T = in.next();
+        int temp = T.charAt(0);
+        in.close();
+        System.out.println(temp);
+    }
+}
