badaed8fadbf48a17b5356524cbeca9c5c80f386
Chang-Jun Jeon <86146128+ondjj@users.noreply.github.com>
2023-06-03 17:44:53 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1652 ms, Memory: 336704 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -1,27 +1,27 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
+        int[] cnt = new int[10001];
 
         int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N];
 
         for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
+            cnt[Integer.parseInt(br.readLine())]++;
         }
+        br.close();
 
-        Arrays.sort(arr);
-        
-        for (int i=0; i<N; i++){
-            sb.append(arr[i]).append('\n');
+        for(int i=1; i<10001; i++){
+            while(cnt[i]>0){
+                sb.append(i).append('\n');
+                cnt[i]--;
+            }
         }
-
         System.out.println(sb);
     }
 }
