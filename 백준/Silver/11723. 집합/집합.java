1ad480bd69450a5e3f9e4fb32a2fbff4a36e0fe5
Chang-Jun Jeon <86146128+ondjj@users.noreply.github.com>
2023-06-15 17:23:33 +0900

    [Silver V] Title: 집합, Time: 1088 ms, Memory: 314920 KB -BaekjoonHub

---
+++ 백준/Silver/11723. 집합/집합.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+        int bitset = 0;
+
+        while(n-- > 0){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String op = st.nextToken();
+            int num;
+
+            switch (op){
+                case "add" :
+                    num = Integer.parseInt(st.nextToken());
+                    bitset |= (1 << (num - 1));
+                    break;
+                case "remove" :
+                    num = Integer.parseInt(st.nextToken());
+                    bitset = bitset & ~(1 << (num - 1));
+                    break;
+                case "check" :
+                    num = Integer.parseInt(st.nextToken());
+                    sb.append((bitset & (1 << (num - 1))) != 0 ? "1\n" : "0\n");
+                    break;
+                case "toggle" :
+                    num = Integer.parseInt(st.nextToken());
+                    bitset ^= (1 << (num - 1));
+                    break;
+                case "all" :
+                    bitset |= (~0);
+                    break;
+                case "empty" :
+                    bitset &= 0;
+                    break;
+            }
+        }
+        bw.write(sb.toString());
+        bw.close();
+        br.close();
+    }
+}
