6418eb91f4c80b8b03f45a3204cc442485f8790b
Chang-Jun Jeon <86146128+ondjj@users.noreply.github.com>
2023-06-18 15:26:41 +0900

    [Silver IV] Title: ATM, Time: 148 ms, Memory: 14552 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int sum = 0;
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j <= i; j++) {
+                sum += arr[j];
+            }
+        }
+
+        System.out.println(sum);
+        bw.close();
+        br.close();
+    }
+}
