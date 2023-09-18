bd0fa5f4ae23677157329a01f350cea2fa9ab933
Chang-Jun Jeon <86146128+ondjj@users.noreply.github.com>
2023-09-18 17:42:58 +0900

    [Silver II] Title: 최소 힙, Time: 1244 ms, Memory: 32952 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> priQueue = new PriorityQueue<>();
+
+        for (int i = 0; i < n; i++) {
+            int temp = Integer.parseInt(br.readLine());
+
+            if (temp == 0) {
+                if (priQueue.isEmpty()) {
+                    System.out.println(0);
+                } else {
+                    System.out.println(priQueue.poll());
+                }
+            } else {
+                priQueue.add(temp);
+            }
+        }
+    }
+}
